d7b7f28a5909052b9d2efd420b59d372a7eaa998
Kashish Rathore <115409116+KashishRathore@users.noreply.github.com>
2023-07-13 02:08:20 +0530

    Update nav.jsx

--- myproject/src/component/nav.jsx
+++ myproject/src/component/nav.jsx
@@ -1,23 +1,7 @@
 import React from "react";
 
-const nav = () => {
-    return(
-    <nav class="navbar">
-    <div class="logo ">
-      <img src="#" alt="" />
-    </div>
-    <div>
-      <ul class="heading">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Shop</a></li>
-        <li><a href="#">Blog</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Contact</a></li>
-        <li><a class="cart" href="cart.html"><i class="fa fa-shopping-cart"></i></a></li>
-      </ul>
-    </div>
-  </nav>
-  );
+const header = () => {
+    return <nav>Get Ready to Complete your Daily Goals</nav>
 };
 
 export default nav;
